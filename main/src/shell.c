392bde1d459b840517f116d0f96c8e36d7afa74b
Tobias Schramm <tobias@t-sys.eu>
2023-10-15 12:42:23 +0200

    shell: simplify declaration of shell commands

--- main/src/shell.c
+++ main/src/shell.c
@@ -1,11 +1,15 @@
 #include "shell.h"
 
 #include <esp_console.h>
+#include <esp_log.h>
 #include <argtable3/argtable3.h>
 
 #include "neighbour.h"
 #include "node_info.h"
 #include "ota.h"
+#include "util.h"
+
+static const char *TAG = "repl";
 
 void main_loop_lock(void);
 void main_loop_unlock(void);
@@ -53,6 +57,23 @@ static int ota_ignore_version(int argc, char **argv) {
 	return 0;
 }
 
+#define ADD_COMMAND(name_, help_, func_) do {		\
+	const esp_console_cmd_t cmd = { 		\
+		.command = (name_),			\
+		.help = (help_),			\
+		.func = &(func_),			\
+		.hint = NULL,				\
+		.argtable = NULL			\
+	};						\
+	esp_err_t err =					\
+		esp_console_cmd_register(&cmd);		\
+	if (err) {					\
+		ESP_LOGE(TAG, "Failed to register %s",	\
+			 (name_));			\
+		return err;				\
+	}						\
+} while (0)
+
 esp_err_t shell_init(void) {
 	esp_console_repl_t *repl = NULL;
 	esp_console_repl_config_t repl_config = ESP_CONSOLE_REPL_CONFIG_DEFAULT();
@@ -60,86 +81,32 @@ esp_err_t shell_init(void) {
 	repl_config.max_cmdline_length = 255;
 	esp_console_register_help_command();
 
-	const esp_console_cmd_t list_neighbours_cmd = {
-		.command = "list_neighbours",
-		.help = "List wireless neighbours",
-		.hint = NULL,
-		.func = &list_neighbours,
-		.argtable = NULL,
-	};
+	ADD_COMMAND("list_neighbours",
+		    "List wireless neighbours",
+		    list_neighbours);
 
-	esp_err_t err = esp_console_cmd_register(&list_neighbours_cmd);
-	if (err) {
-		return err;
-	}
+	ADD_COMMAND("serve_ota",
+		    "Serve own firmware via OTA update to neighbours",
+		    serve_ota);
 
-	const esp_console_cmd_t serve_ota_cmd = {
-		.command = "serve_ota",
-		.help = "Serve own firmware via OTA update to neighbours",
-		.hint = NULL,
-		.func = &serve_ota,
-		.argtable = NULL,
-	};
+	ADD_COMMAND("stop_serving_ota",
+		    "Stop serving own firmware via OTA",
+		    stop_serving_ota);
 
-	err = esp_console_cmd_register(&serve_ota_cmd);
-	if (err) {
-		return err;
-	}
+	ADD_COMMAND("node_info",
+		    "Show information about local node",
+		    node_info);
 
-	const esp_console_cmd_t stop_serving_ota_cmd = {
-		.command = "stop_serving_ota",
-		.help = "Stop serving own firmware via OTA",
-		.hint = NULL,
-		.func = &stop_serving_ota,
-		.argtable = NULL,
-	};
+	ADD_COMMAND("ota_status",
+		    "Show OTA update status",
+		    ota_status);
 
-	err = esp_console_cmd_register(&stop_serving_ota_cmd);
-	if (err) {
-		return err;
-	}
-
-	const esp_console_cmd_t node_info_cmd = {
-		.command = "node_info",
-		.help = "Show information on local node",
-		.hint = NULL,
-		.func = &node_info,
-		.argtable = NULL,
-	};
-
-	err = esp_console_cmd_register(&node_info_cmd);
-	if (err) {
-		return err;
-	}
-
-	const esp_console_cmd_t ota_status_cmd = {
-		.command = "ota_status",
-		.help = "Show OTA update status",
-		.hint = NULL,
-		.func = &ota_status,
-		.argtable = NULL,
-	};
-
-	err = esp_console_cmd_register(&ota_status_cmd);
-	if (err) {
-		return err;
-	}
-
-	const esp_console_cmd_t ota_ignore_version_cmd = {
-		.command = "ota_ignore_version",
-		.help = "Show OTA update status",
-		.hint = NULL,
-		.func = &ota_ignore_version,
-		.argtable = NULL,
-	};
-
-	err = esp_console_cmd_register(&ota_ignore_version_cmd);
-	if (err) {
-		return err;
-	}
+	ADD_COMMAND("ota_ignore_version",
+		    "Force OTA update even if version already installed",
+		    ota_ignore_version);
 
 	esp_console_dev_usb_serial_jtag_config_t hw_config = ESP_CONSOLE_DEV_USB_SERIAL_JTAG_CONFIG_DEFAULT();
-	err = esp_console_new_repl_usb_serial_jtag(&hw_config, &repl_config, &repl);
+	esp_err_t err = esp_console_new_repl_usb_serial_jtag(&hw_config, &repl_config, &repl);
 	if (err) {
 		return err;
 	}
