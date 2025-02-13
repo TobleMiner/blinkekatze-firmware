f0fe3e256413212e0da2e8e69e386bceb9837228
Tobias Schramm <tobias@t-sys.eu>
2025-02-14 00:57:34 +0100

    color_palette: add aurora palettes

--- main/src/color_palette.c
+++ main/src/color_palette.c
@@ -26,3 +26,32 @@ const color_palette_t rainbow_stripe_palette = {
 	.num_colors = ARRAY_SIZE(rainbow_stripe_colors),
 	.color_maxval = 0xff
 };
+
+static const color_palette_color_t aurora_colors[] = {
+	{ { 1, 5, 45 }, 1 },
+	{ { 0, 200, 23 }, 1 },
+	{ { 0, 255, 0 }, 1 },
+	{ { 0, 243, 45 }, 1 },
+	{ { 0, 135, 7 }, 1 },
+	{ { 1, 5, 45 }, 1 },
+};
+
+const color_palette_t aurora_palette = {
+	.colors = aurora_colors,
+	.num_colors = ARRAY_SIZE(aurora_colors),
+	.color_maxval = 0xff
+};
+
+static const color_palette_color_t aurora2_colors[] = {
+	{ { 17, 177, 13 }, 1 },
+	{ { 121, 142, 5 }, 1 },
+	{ { 25, 173, 121 }, 1 },
+	{ { 250, 77, 127 }, 1 },
+	{ { 171, 101, 221 }, 1 },
+};
+
+const color_palette_t aurora2_palette = {
+	.colors = aurora2_colors,
+	.num_colors = ARRAY_SIZE(aurora2_colors),
+	.color_maxval = 0xff
+};
