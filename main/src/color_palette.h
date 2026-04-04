49b4ce6a7f3131e08f43096bfff539c02e53bdf5
Annika Schramm <annika@t-sys.eu>
2026-04-04 17:35:43 +0200

    color_palette: support different opaque palette data formats
    Palette data comes in different forms. Add ops struct to support parsing
    and operating on data in different formats.

--- main/src/color_palette.h
+++ main/src/color_palette.h
@@ -1,18 +1,37 @@
 #pragma once
 
+#include <stdint.h>
+
 #include "color.h"
 
 typedef struct color_palette_color {
 	rgb16_t color;
-	unsigned int duration;
+	unsigned int position;
 } color_palette_color_t;
 
+typedef struct color_palette_ops color_palette_ops_t;
+
 typedef struct color_palette {
-	const color_palette_color_t *colors;
+	const void *data;
+	const color_palette_ops_t *ops;
 	unsigned int num_colors;
 	unsigned int color_maxval;
 } color_palette_t;
 
+struct color_palette_ops {
+	unsigned int (*get_total_length)(const color_palette_t *palette);
+	void (*get_color_at)(color_t *color, const color_palette_t *palette, unsigned int pos);
+};
+
 extern const color_palette_t rainbow_stripe_palette;
 extern const color_palette_t aurora_palette;
 extern const color_palette_t aurora2_palette;
+extern const color_palette_t autumn_palette;
+
+static unsigned int color_palette_get_total_length(const color_palette_t *palette) {
+	return palette->ops->get_total_length(palette);
+}
+
+static void color_palette_get_color_at(color_t *color, const color_palette_t *palette, unsigned int pos) {
+	palette->ops->get_color_at(color, palette, pos);
+}
